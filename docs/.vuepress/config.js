c3ff289ede23090624845321b46f47f5cdbdbb0e
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-03-26 22:55:25 -0400

    WIP

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,15 @@ module.exports = {
       href: '/favicon.ico'
     }
   ],
-  ga: 'UA-115585378-1',
+
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-115585378-1'
+      }
+    ]
+  ],
   themeConfig: {
     repo: 'PrefectHQ/prefect',
     docsDir: 'docs',
@@ -242,10 +250,8 @@ module.exports = {
       ]
     }
   },
-  markdown: {
-    config: md => {
-      md.use(require('markdown-it-attrs'))
-      md.use(require('markdown-it-checkbox'))
-    }
+  extendMarkdown: md => {
+    md.use(require('markdown-it-attrs'))
+    md.use(require('markdown-it-checkbox'))
   }
 }
